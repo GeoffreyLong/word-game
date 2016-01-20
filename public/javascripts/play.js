fd1cff76ae18899657a33b4a57162e76966ae537
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-01-19 21:38:46 -0500

    Add initial game mechanics

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+  // The selected letter element
+  var selected = null;
+
   $('#board').on('click', '.boardElm', function(){
-    console.log('boardElm');
+    if ($(this).text() != ""){
+      console.log('Contains a letter');
+    }
+    else{
+      $(this).text($(selected).text());
+      $(selected).remove();
+      selected = null;
+    }
   });
   
   $('#letters').on('click', '.letterElm', function(){
-    console.log('letterElm');
+    selected = $(this);
   });
 });
