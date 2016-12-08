eda444c6f9ec1222bb60059cbb3f72abd1ac5bbb
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-12-07 19:52:39 -0500

    ADD initial construction

--- src/App.js
+++ src/App.js
@@ -2,20 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="Header">
+          header will be here
+          will contain the levels... I guess
+          maybe some concept of score
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Game />
       </div>
     );
   }
 }
+
+// This should probably carry all of the state logic
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <GameBoard />
+        <LetterBoard />
+      </div>
+    );
+  }
+}
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div className="GameBoard">
+        <Letter letter="s" />
+      </div>
+    );
+  }
+}
+
+class LetterBoard extends Component {
+  render() {
+    return (
+      <div className="LetterBoard">
+        <Letter letter="s" />
+      </div>
+    );
+  }
+}
+
+class Letter extends Component {
+  render() {
+    return (
+      <div className="Letter">
+        {this.props.letter}
+      </div>
+    );
+  }
+}
+
 
 export default App;
