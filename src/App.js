ddb6cad7c3b0a6ce4ed94a5148a9bea3b5fdb304
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-12-08 20:14:33 -0500

    ADD initial button logic routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// TODO
+//
+// POSSIBLE OPTIMIZATIONS
+//    Draggable / Droppable for the letters
+//        Would need separate classes for gameButtons and letterButtons
+//    A trie for the word checking
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -20,31 +27,53 @@ class App extends Component {
 
 // This should probably carry all of the state logic
 class Game extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      board: [
+        ['', '', '', '', ''],
+        ['', 'a', 'b', 'c', ''],
+        ['', 'a', 'b', 'c', ''],
+      ],
+      letters: ['a', 'b', 'c']
+    };
+  }
+  handleLBClick(letterIdx) {
+    alert("ok: " + letterIdx);
+  }
+  handleGBClick(letter, row) {
+    alert("ok2: " + letter + " " + row);
+  }
+  handleUndoClick() {
+    alert("undo");
+  }
+  handleSubmitClick() {
+    alert("submit");
+  }
   render() {
-    var testBoard = [
-      ['', '', '', '', ''],
-      ['', 'a', 'b', 'c', ''],
-      ['', 'a', 'b', 'c', ''],
-    ];
-    var testLetters = ['a', 'b', 'c'];
     return (
       <div className="Game">
-        <GameBoard letters={testBoard}/>
-        <LetterBoard letters={testLetters}/>
+        <GameBoard  letters={this.state.board}
+                    handleGBClick={(letter, row) => this.handleGBClick(letter, row)}/>
+        <LetterBoard  letters={this.state.letters} 
+                      handleLBClick={(letterIdx) => this.handleLBClick(letterIdx)}
+                      handleUndoClick={() => this.handleUndoClick()}
+                      handleSubmitClick={() => this.handleSubmitClick()}/>
       </div>
     );
   }
 }
 
 class GameBoard extends Component {
-  renderRow(letters) {
-    return <BoardRow letters={letters} />
+  renderRow(letters, row) {
+    return <BoardRow letters={letters} handleGBClick={(letter) => this.props.handleGBClick(letter, row)} />
   }
   render() {
     var table = [];
-    this.props.letters.forEach((letterRow) => {
+    this.props.letters.forEach((letterRow, row) => {
       console.log(letterRow);
-      table.push(this.renderRow(letterRow));
+      table.push(this.renderRow(letterRow, row));
     });
     console.log(table);
     return (
@@ -56,13 +85,13 @@ class GameBoard extends Component {
 }
 
 class BoardRow extends Component {
-  renderLetter(letter) {
-    return <Letter letter={letter} />
+  renderLetter(letter, letterIdx) {
+    return <Letter letter={letter} handleClick={() => this.props.handleGBClick(letterIdx)}  />;
   }
   render() {
     var letters = [];
-    this.props.letters.forEach((letter) => {
-      letters.push(this.renderLetter(letter));
+    this.props.letters.forEach((letter, letterIdx) => {
+      letters.push(this.renderLetter(letter, letterIdx));
     });
     return (
       <div className="BoardRow">
@@ -73,17 +102,20 @@ class BoardRow extends Component {
 }
 
 class LetterBoard extends Component {
-  renderLetter(letter) {
-    return <Letter letter={letter} />;
+  renderLetter(letter, letterIdx) {
+    return <Letter letter={letter} handleClick={() => this.props.handleLBClick(letterIdx)}  />;
   }
   render() {
     var letters = [];
-    this.props.letters.forEach((letter) => {
-      letters.push(this.renderLetter(letter));
+    this.props.letters.forEach((letter, letterIdx) => {
+      letters.push(this.renderLetter(letter, letterIdx));
     });
     return (
       <div className="LetterBoard">
         {letters}
+        <div> Letter Selected: TODO </div>
+        <GameButtons  handleUndoClick={() => this.props.handleUndoClick()}
+                      handleSubmitClick={() => this.props.handleSubmitClick()}/>
       </div>
     );
   }
@@ -92,9 +124,20 @@ class LetterBoard extends Component {
 class Letter extends Component {
   render() {
     return (
-      <button className="Letter">
+      <button className="Letter" onClick={() => this.props.handleClick()}>
         {this.props.letter}
       </button>
+    );
+  }
+}
+
+class GameButtons extends Component {
+  render() {
+    return (
+      <div className="GameButtons">
+        <button onClick={() => this.props.handleUndoClick()}> Undo </button>
+        <button onClick={() => this.props.handleSubmitClick()}> Submit </button>
+      </div>
     );
   }
 }
